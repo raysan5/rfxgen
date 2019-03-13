fd40a6fd7f9bf97288f1366ee0be869f4504ff96
Ray <raysan5@gmail.com>
2019-03-13 13:24:03 +0100

    Added miniaudio to about

--- src/gui_window_about.h
+++ src/gui_window_about.h
@@ -129,7 +129,7 @@ GuiWindowAboutState InitGuiWindowAbout(void)
 
     // Custom variables initialization
     state.windowWidth = 335;
-    state.windowHeight = 340;
+    state.windowHeight = 340 + 25;
     state.position = (Vector2){ GetScreenWidth()/2 - state.windowWidth/2, GetScreenHeight()/2 - state.windowHeight/2 };
 
     return state;
@@ -164,7 +164,7 @@ void GuiWindowAbout(GuiWindowAboutState *state)
 
         DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)), 0.85f));
 
-        state->windowAboutActive = !GuiWindowBox((Rectangle){ state->position.x + 0, state->position.y + 0, 335, 340 }, windowAboutText);
+        state->windowAboutActive = !GuiWindowBox((Rectangle){ state->position.x + 0, state->position.y + 0, 335, 340 + 25 }, windowAboutText);
 
         // Draw a background rectangle for convenience
         DrawRectangle(state->position.x + 1, state->position.y + 4 + 20, state->windowWidth - 2, 90 - 4, Fade(GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_NORMAL)), 0.5f));
@@ -187,21 +187,24 @@ void GuiWindowAbout(GuiWindowAboutState *state)
         if (GuiLabelButton((Rectangle){ state->position.x + 155, state->position.y + 135, 80, 15 }, linkraylibText)) { OpenURL("https://www.raylib.com/"); }
         if (GuiLabelButton((Rectangle){ state->position.x + 155, state->position.y + 160, 150, 15 }, linkGitraylibText)) { OpenURL("https://github.com/raysan5/raylib"); }
         if (GuiLabelButton((Rectangle){ state->position.x + 155, state->position.y + 180, 150, 15 }, linkGitrayguiText)) { OpenURL("https://github.com/raysan5/raygui"); }
+        
+        DrawTextEx(GetFontDefault(), "miniAUDIO", (Vector2){ state->position.x + 12, state->position.y + 205 }, 30, 2, GetColor(0x003800ff));
+        if (GuiLabelButton((Rectangle){ state->position.x + 155, state->position.y + 210, 150, 15 }, "github.com/dr-soft/miniaudio")) { OpenURL("https://github.com/dr-soft/miniaudio"); }
 
-        GuiLine((Rectangle){ state->position.x, state->position.y + 200, 335, 20 }, NULL);
+        GuiLine((Rectangle){ state->position.x, state->position.y + 200 + 30, 335, 20 }, NULL);
 
-        GuiLabel((Rectangle){ state->position.x + 10, state->position.y + 220, 289, 20 }, lblCopyrightText);
-        if (GuiLabelButton((Rectangle){ state->position.x + 215, state->position.y + 220, 60, 20 }, linkraylibtechText)) { OpenURL("https://twitter.com/raylibtech"); }
+        GuiLabel((Rectangle){ state->position.x + 10, state->position.y + 220 + 28, 289, 20 }, lblCopyrightText);
+        if (GuiLabelButton((Rectangle){ state->position.x + 215, state->position.y + 220 + 28, 60, 20 }, linkraylibtechText)) { OpenURL("https://twitter.com/raylibtech"); }
 
-        GuiLabel((Rectangle){ state->position.x + 10, state->position.y + 250, 65, 15 }, lblMoreInfoText);
+        GuiLabel((Rectangle){ state->position.x + 10, state->position.y + 250 + 25, 65, 15 }, lblMoreInfoText);
 
-        if (GuiLabelButton((Rectangle){ state->position.x + 80, state->position.y + 250, 165, 15 }, linkToolWebText)) { OpenURL("https://www.raylibtech.com/"); }
-        if (GuiLabelButton((Rectangle){ state->position.x + 80, state->position.y + 270, 165, 15 }, linkMailText)) { OpenURL("mailto:ray@raylibtech.com"); }
+        if (GuiLabelButton((Rectangle){ state->position.x + 80, state->position.y + 250 + 25, 165, 15 }, linkToolWebText)) { OpenURL("https://www.raylibtech.com/"); }
+        if (GuiLabelButton((Rectangle){ state->position.x + 80, state->position.y + 270 + 25, 165, 15 }, linkMailText)) { OpenURL("mailto:ray@raylibtech.com"); }
 
-        GuiLabel((Rectangle){ state->position.x + 10, state->position.y + 270, 65, 15 }, lblSupportText);
-        GuiLine((Rectangle){ state->position.x, state->position.y + 285, 335, 20 }, NULL);
+        GuiLabel((Rectangle){ state->position.x + 10, state->position.y + 270 + 25, 65, 15 }, lblSupportText);
+        GuiLine((Rectangle){ state->position.x, state->position.y + 285 + 25, 335, 20 }, NULL);
 
-        DrawRectangle(state->position.x + 1, state->position.y + 285 + 11, state->windowWidth - 2, 43, Fade(GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_NORMAL)), 0.5f));
+        DrawRectangle(state->position.x + 1, state->position.y + 285 + 11 + 25, state->windowWidth - 2, 43, Fade(GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_NORMAL)), 0.5f));
 
         GuiSetStyle(BUTTON, TEXT_ALIGNMENT, 1);
         GuiSetStyle(BUTTON, INNER_PADDING, 1);
@@ -210,9 +213,9 @@ void GuiWindowAbout(GuiWindowAboutState *state)
         //state->chkLicenseChecked = GuiCheckBox((Rectangle){ state->position.x + 10, state->position.y + 310, 16, 16 }, chkLicenseText, state->chkLicenseChecked);
 #else
         //GuiDisable();state->chkLicenseChecked = GuiCheckBox((Rectangle){ state->position.x + 10, state->position.y + 310, 16, 16 }, chkLicenseText, state->chkLicenseChecked); GuiEnable();
-        if (GuiButton((Rectangle){ state->position.x + 175, state->position.y + 305, 75, 25 }, BtnBeONEText)) { OpenURL("https://raylibtech.itch.io/rfxgen"); }
+        if (GuiButton((Rectangle){ state->position.x + 175, state->position.y + 305 + 25, 75, 25 }, BtnBeONEText)) { OpenURL("https://raylibtech.itch.io/rfxgen"); }
 #endif
-        if (GuiButton((Rectangle){ state->position.x + 255, state->position.y + 305, 70, 25 }, BtnCloseText)) state->windowAboutActive = false;
+        if (GuiButton((Rectangle){ state->position.x + 255, state->position.y + 305 + 25, 70, 25 }, BtnCloseText)) state->windowAboutActive = false;
         GuiSetStyle(BUTTON, TEXT_ALIGNMENT, 0);
 
         GuiDisable();
