bd72b76401ada5389cfa59c4b94e59c98b3f97f0
Ray <raysan5@gmail.com>
2024-07-21 18:06:43 +0200

    Update gui_window_help.h

--- src/gui_window_help.h
+++ src/gui_window_help.h
@@ -141,7 +141,7 @@ GuiWindowHelpState InitGuiWindowHelp(void)
     state.windowActive = false;
     state.supportDrag = false;
 
-    state.windowBounds = (Rectangle){ (float)GetScreenWidth()/2 - 360/2, 0, 360, 0 };
+    state.windowBounds = (Rectangle){ (float)GetScreenWidth()/2 - 320/2, 0, 320, 0 };
     state.panOffset = (Vector2){ 0, 0 };
     state.dragMode = false;
 
@@ -211,17 +211,33 @@ void GuiWindowHelp(GuiWindowHelpState *state)
         }
         //----------------------------------------------------------------------------------------
 
+        // Update window bounds and size: center to screen and allow scrolling
+        //----------------------------------------------------------------------------------------
+        state->windowBounds.x = GetScreenWidth()/2 - state->windowBounds.width/2;
+        state->windowBounds.height = (float)(state->contentHeight + 24 + 4);
+        state->windowBounds.y = GetScreenHeight()/2 - state->windowBounds.height/2;
+
+        // Review size if it does not fit on the screen
+        if (state->windowBounds.height > (GetScreenHeight() - 80))
+        {
+            state->windowBounds.height = (float)GetScreenHeight() - 80;
+            state->windowBounds.y = GetScreenHeight()/2 - state->windowBounds.height/2;
+        }
+        //----------------------------------------------------------------------------------------
+
         // Draw window and controls
         //----------------------------------------------------------------------------------------
-        state->windowActive = !GuiWindowBox(state->windowBounds, TextFormat("#193#%s Help", TOOL_NAME));      
+        state->windowActive = !GuiWindowBox(state->windowBounds, TextFormat("#193#%s Help", TOOL_NAME));
 
-        int nextLineY = 0;
-        nextLineY += (GUIHELPWINDOW_LINE_HEIGHT + 4);
+        int nextLineY = (GUIHELPWINDOW_LINE_HEIGHT + 4);
 
-        // Draw scroll panel considering window bounds and content size
+        // Draw scroll panel considering window bounds and content size (only if required)
         Rectangle scissor = { 0 };
-        GuiScrollPanel((Rectangle){ state->windowBounds.x, state->windowBounds.y + GUIHELPWINDOW_LINE_HEIGHT - 1, state->windowBounds.width, state->windowBounds.height - 24 + 1 }, NULL,
+        if (state->contentHeight > (state->windowBounds.height - 24))
+        {
+            GuiScrollPanel((Rectangle){ state->windowBounds.x, state->windowBounds.y + GUIHELPWINDOW_LINE_HEIGHT - 1, state->windowBounds.width, state->windowBounds.height - 24 + 1 }, NULL,
                        (Rectangle){ state->windowBounds.x, state->windowBounds.y + GUIHELPWINDOW_LINE_HEIGHT, state->windowBounds.width - 16, (float)state->contentHeight }, &state->scrollPanelOffset, &scissor);
+        }
 
         // Draw help info and separation lines
         // WARNING: We only scissor if scrolling is required, scissor mode forces a new draw call
@@ -240,7 +256,6 @@ void GuiWindowHelp(GuiWindowHelpState *state)
 
         if (state->contentHeight > (state->windowBounds.height - 24)) EndScissorMode();
     }
-    else state->windowBounds = (Rectangle){ GetScreenWidth()/2 - state->windowBounds.width/2, GetScreenHeight()/2 - state->windowBounds.height/2, state->windowBounds.width, state->windowBounds.height };
 }
 
 #endif // GUI_WINDOW_HELP_IMPLEMENTATION
